ba30d9219bb541118a465c30688d1b4f2752de38
jmansoEvolutio <163314953+jmansoEvolutio@users.noreply.github.com>
2024-05-31 01:39:16 +0200

    Update horarios.py

--- horarios.py
+++ horarios.py
@@ -6,12 +6,12 @@ archivo_xlsx = 'datos_distancia.xlsx'
 df = pd.read_excel(archivo_xlsx)
 
 # Crear una lista desplegable con las columnas del DataFrame
-columna_seleccionada = st.selectbox("Selecciona una columna:", df.columns)
+#columna_seleccionada = st.selectbox("Selecciona una columna:", df.columns)
 
 # Filtrar los datos según la columna seleccionada
-datos_filtrados = df[columna_seleccionada]
+#datos_filtrados = df[columna_seleccionada]
 
 # Mostrar los datos filtrados en una tabla
-st.write(f"Datos de la columna '{columna_seleccionada}':")
-st.write(datos_filtrados)
+#st.write(f"Datos de la columna '{columna_seleccionada}':")
+#st.write(datos_filtrados)
 
