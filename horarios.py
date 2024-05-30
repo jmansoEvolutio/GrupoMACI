854846c85adb147b90cd77fe35f54b3b1e62e96f
jmansoEvolutio <163314953+jmansoEvolutio@users.noreply.github.com>
2024-05-31 01:35:49 +0200

    Update horarios.py

--- horarios.py
+++ horarios.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import streamlit as st
+
+# Cargar el archivo XLSX (reemplaza 'mi_archivo.xlsx' con la ruta de tu archivo)
+archivo_xlsx = 'datos_distancia.xlsx'
+df = pd.read_excel(archivo_xlsx)
+
+# Crear una lista desplegable con las columnas del DataFrame
+columna_seleccionada = st.selectbox("Selecciona una columna:", df.columns)
+
+# Filtrar los datos según la columna seleccionada
+datos_filtrados = df[columna_seleccionada]
+
+# Mostrar los datos filtrados en una tabla
+st.write(f"Datos de la columna '{columna_seleccionada}':")
+st.write(datos_filtrados)
 
