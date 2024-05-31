43c52ba502a26b9b3968280c5080bf8892d179be
jmansoEvolutio <163314953+jmansoEvolutio@users.noreply.github.com>
2024-05-31 12:09:59 +0200

    Update horarios.py

--- horarios.py
+++ horarios.py
@@ -28,14 +28,14 @@ file_path = 'datos_distancia.xlsx'  # Reemplaza con la ruta de tu archivo xlsx
 data = pd.read_excel(file_path)
 
 # Crea una lista de diccionarios, cada uno con los datos de una fila
-dropdown_options = data.to_dict('records')
+#dropdown_options = data.to_dict('records')
 
 # Crea un menú desplegable para seleccionar una fila del archivo xlsx
-selected_option = st.selectbox('Selecciona una fila:', dropdown_options)
+#selected_option = st.selectbox('Selecciona una fila:', dropdown_options)
 
 # Muestra los detalles de la opción seleccionada
-st.write('Detalles de la fila seleccionada:')
-st.json(selected_option)
+#st.write('Detalles de la fila seleccionada:')
+#st.json(selected_option)
 
 # Crea una tabla para mostrar los datos de todas las filas seleccionadas
 selected_rows = st.multiselect('Selecciona filas adicionales:', dropdown_options)
