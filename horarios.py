2774e9ce353fa53a9c3ca10615f7679efd1cd3fe
jmansoEvolutio <163314953+jmansoEvolutio@users.noreply.github.com>
2024-05-31 11:39:48 +0200

    Update horarios.py

--- horarios.py
+++ horarios.py
@@ -1,6 +1,9 @@
-import pandas as pd
+#import pandas as pd
+#import streamlit as st
+#import os
+
 import streamlit as st
-import os
+import pandas as pd
 
 # Cargar el archivo XLSX
 @st.cache
@@ -10,14 +13,17 @@ def cargar_datos():
 
 df = cargar_datos()
 
-# Crear una lista desplegable con los valores concatenados de cada fila
-fila_seleccionada = st.selectbox("Selecciona una fila:", df.index)
-valores_fila = df.iloc[fila_seleccionada].values
-valores_concatenados = " | ".join(map(str, valores_fila))
+# Crear una lista desplegable con los valores concatenados de cada columna
+columnas_concatenadas = []
+for columna in df.columns:
+    valores_columna = df[columna].values
+    valores_concatenados = " | ".join(map(str, valores_columna))
+    columnas_concatenadas.append(valores_concatenados)
 
-st.write(f"Valores seleccionados: {valores_concatenados}")
+fila_seleccionada = st.selectbox("Selecciona una fila:", columnas_concatenadas)
 
 # Crear una tabla para mostrar los datos seleccionados
 st.write("Datos seleccionados:")
-st.table(df.loc[df.index.isin(range(fila_seleccionada + 1))])
+st.table(df.loc[df.apply(lambda row: any(val in fila_seleccionada for val in row.values), axis=1)])
+
 
