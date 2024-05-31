83df011a1bc44b01fe63f30247bee12260a9d9d8
jmansoEvolutio <163314953+jmansoEvolutio@users.noreply.github.com>
2024-05-31 11:59:13 +0200

    Update horarios.py

--- horarios.py
+++ horarios.py
@@ -1,13 +1,13 @@
+
 import streamlit as st
 import pandas as pd
 
 # Carga el archivo xlsx
 file_path = 'datos_distancia.xlsx'  # Reemplaza con la ruta de tu archivo xlsx
-data = pd.read_excel(file_path) 
+data = pd.read_excel(file_path)
 
 # Crea una lista de diccionarios, cada uno con los datos de una fila
 dropdown_options = data.to_dict('records')
-
 
 # Crea un menú desplegable para seleccionar una fila del archivo xlsx
 selected_option = st.selectbox('Selecciona una fila:', dropdown_options)
@@ -15,3 +15,7 @@ selected_option = st.selectbox('Selecciona una fila:', dropdown_options)
 # Muestra los detalles de la opción seleccionada
 st.write('Detalles de la fila seleccionada:')
 st.json(selected_option)
+
+# Crea una tabla para mostrar los datos de la fila seleccionada
+st.write('Tabla de datos:')
+st.table(pd.DataFrame(selected_option))
