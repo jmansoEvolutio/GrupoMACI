b4e11c94aa22af1ee1fe1248e09802cb3bcfa160
jmansoEvolutio <163314953+jmansoEvolutio@users.noreply.github.com>
2024-05-31 11:30:33 +0200

    Update horarios.py

--- horarios.py
+++ horarios.py
@@ -5,17 +5,16 @@ import os
 # Cargar el archivo XLSX
 @st.cache
 def load_data():
-    df = pd.read_excel('datos_distancia.xlsx')
+    df = pd.read_excel('ruta/a/tu/archivo.xlsx')
     return df
 
 df = load_data()
 
-# Crear una lista desplegable con valores únicos de la columna 'client_id'
-client_ids = df['client_id'].unique()
-selected_client_id = st.selectbox('Selecciona un client_id:', client_ids)
+# Crear una lista desplegable con todos los valores de la fila
+selected_row = st.selectbox('Selecciona una fila:', df.index)
 
-# Filtrar los datos según el client_id seleccionado
-filtered_data = df[df['client_id'] == selected_client_id]
+# Filtrar los datos según la fila seleccionada
+filtered_data = df.loc[selected_row]
 
 # Mostrar los datos en una tabla
 st.table(filtered_data)
@@ -23,4 +22,5 @@ st.table(filtered_data)
 # Acumular los datos seleccionados en una tabla
 st.write('Datos seleccionados:')
 st.dataframe(filtered_data)
+
 
