d5b0b729f69cc438d241be42e05b61f87471161e
jmansoEvolutio <163314953+jmansoEvolutio@users.noreply.github.com>
2024-05-31 12:08:31 +0200

    Update horarios.py

--- horarios.py
+++ horarios.py
@@ -1,3 +1,25 @@
+#import streamlit as st
+#import pandas as pd
+
+# Carga el archivo xlsx
+#file_path = 'datos_distancia.xlsx'  # Reemplaza con la ruta de tu archivo xlsx
+#data = pd.read_excel(file_path)
+
+# Crea una lista de diccionarios, cada uno con los datos de una fila
+#dropdown_options = data.to_dict('records')
+
+# Crea un menú desplegable para seleccionar una fila del archivo xlsx
+#selected_option = st.selectbox('Selecciona una fila:', dropdown_options)
+
+# Muestra los detalles de la opción seleccionada
+#st.write('Detalles de la fila seleccionada:')
+#st.json(selected_option)
+
+# Crea una tabla para mostrar los datos de la fila seleccionada
+#st.write('Tabla de datos:')
+#st.table(pd.DataFrame(selected_option, index=[0]))
+
+
 import streamlit as st
 import pandas as pd
 
@@ -14,10 +36,6 @@ selected_option = st.selectbox('Selecciona una fila:', dropdown_options)
 # Muestra los detalles de la opción seleccionada
 st.write('Detalles de la fila seleccionada:')
 st.json(selected_option)
-
-# Crea una tabla para mostrar los datos de la fila seleccionada
-#st.write('Tabla de datos:')
-#st.table(pd.DataFrame(selected_option, index=[0]))
 
 # Crea una tabla para mostrar los datos de todas las filas seleccionadas
 selected_rows = st.multiselect('Selecciona filas adicionales:', dropdown_options)
