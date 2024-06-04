aca0d60520e7b18dba29f28318056a037f2b9798
jmansoEvolutio <163314953+jmansoEvolutio@users.noreply.github.com>
2024-06-04 09:59:35 +0200

    Update horarios.py

--- horarios.py
+++ horarios.py
@@ -32,6 +32,6 @@ with right_column:
         st.write('Comunidades seleccionadas:')
         st.dataframe(pd.DataFrame(selected_rows, columns=selected_columns), width=None)
 
-        # Suma los datos de la columna "tiempo"
+       # Suma los datos de la columna "tiempo"
         total_tiempo = sum(row['tiempo'] for row in selected_rows)
         st.write(f'Tiempo total: **{total_tiempo}**')
